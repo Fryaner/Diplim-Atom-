2b5197c4d5415e43032bb900afc017a775c4ca2f
Ilya <ilya.orlov123@mail.ru>
2024-05-18 23:50:39 +1000

    add: Сделаал регистрацию/валидацию формы. И добавления пользователя в БД после регистрациис помощью API

--- client/src/models/userModel.ts
+++ client/src/models/userModel.ts
@@ -2,6 +2,8 @@ export interface UserModel {
     id?: string,
     lastName: string,
     firstName: string,
+    patronymic: string,
+    phone: string,
     login: string,
     email: string, 
     password: string,
--- client/src/modules/register/components/registerForm/index.tsx
+++ client/src/modules/register/components/registerForm/index.tsx
@@ -1,51 +1,190 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "../../../../UI/Button";
 import { Input } from "../../../../UI/Input";
-import { Label } from "../../../../UI/Label";
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { registerSchema } from "../scheme";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "../../../../UI/Form/index";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useAddUserMutation } from "../../api/registerApi";
+import { Spinner } from "@radix-ui/themes";
+
 
 export const RegisterForm = () => {
+    const [addUser, {data, isLoading, isError}] = useAddUserMutation();
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+          login: "",
+          lastName: "",
+          firstName: "",
+          password: "",
+          confirmPassword: "",
+          patronymic: "",
+          email: "",
+          phone: "",
+        },
+      })
+
+    function onSubmit(values: z.infer<typeof registerSchema>) {
+        addUser({
+            firstName: values.firstName,
+            lastName: values.lastName,
+            patronymic: values.lastName,
+            phone: values.phone,
+            email: values.email,
+            login: values.login,
+            password: values.password,
+        })
+    }
+
     return (
         <div className="flex flex-col items-center gap-2 py-[16px] max-md:px-[16px]">
             <h1 className="font-bold" >Регистрация</h1>
-            <form className="flex flex-col w-[50%] gap-4 max-md:w-full">
-                <div className="flex flex-col gap-2">
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col w-[50%] gap-4 max-md:w-full">
+                <div className="flex flex-col gap-4">
                     <div>
-                        <Label htmlFor="lastName">Фамилия<span className="text-[red]">*</span></Label>
-                        <Input  type="text" id="lastName"></Input>
+                        <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Фамилия<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="text" placeholder="Фамилия" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />
                     </div>
                     <div>
-                        <Label htmlFor="firstName">Имя<span className="text-[red]">*</span></Label>
-                        <Input type="text" id="firstName"></Input>                    
+                    <div>
+                        <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Имя<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="text" placeholder="Имя" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />
+                    </div>                 
                     </div>
                     <div>
-                        <Label htmlFor="patronymic">Отчество<span className="text-[red]">*</span></Label>
-                        <Input type="text" id="patronymic"></Input>
+                    <div>
+                        <FormField
+                        control={form.control}
+                        name="patronymic"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Отчество<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="text" placeholder="Отчество" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />
+                    </div>
                     </div>
                     <div>
-                        <Label htmlFor="username">Логин<span className="text-[red]">*</span></Label>
-                        <Input type="text" id="username"></Input>
+                    <FormField
+                        control={form.control}
+                        name="login"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Логин<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="text" placeholder="Логин" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />
                     </div>
                     <div>
-                        <Label htmlFor="phone">Номер телефона<span className="text-[red]">*</span></Label>
-                        <Input type="tel" id="phone"></Input>                   
+                    <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Номер телефона<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="tel" placeholder="Номер телефона" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />              
                     </div>
                     <div>
-                        <Label htmlFor="email">Электронный адрес<span className="text-[red]">*</span></Label>
-                        <Input type="email" id="email"></Input>
+                        <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Электронный адрес<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="email" placeholder="Электронный адрес" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />  
                     </div>
                     <div>
-                        <Label htmlFor="password">Пароль<span className="text-[red]">*</span></Label>
-                        <Input type="password" id="password"></Input>
+                        <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Пароль<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="password" placeholder="Пароль" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />  
                     </div>
                     <div>
-                        <Label htmlFor="repeatPassword">Повторите пароль<span className="text-[red]">*</span></Label>
-                        <Input type="password" id="repeatPassword"></Input>
+                    <FormField
+                        control={form.control}
+                        name="confirmPassword"
+                        render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Подветрдите пароль<span className="text-[red]">*</span></FormLabel>
+                        <FormControl>
+                            <Input type="password" placeholder="Подтвердите пароль" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                        </FormItem>
+                        )}
+                        />  
                     </div>
                 </div>
                 <Button className="bg-[#8761D9]">Зарегистрироваться</Button>
             </form>
+            </Form>
             <div>
                 <NavLink to="../auth" className="underline text-[#8761D9]">Уже есть аккаунт?</NavLink>
+            </div>
+            <div>
+                {isLoading ? <Spinner/> : isError ? <p>Ошибка</p> : data}
             </div>
         </div>
     )
